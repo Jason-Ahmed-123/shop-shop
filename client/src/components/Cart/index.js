456ee987d1c63f030d8ff44b32b70b4b3889a5c6
Jason Ahmed <jason_ahmed@yahoo.com>
2020-10-17 15:25:33 -0700

    fixed redux refactoring

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -9,7 +9,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 
-// API key:
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -16,7 +16,6 @@ export const QUERY_PRODUCTS = gql`
   }
 `;
 
-// Set up the checkout query:
 export const QUERY_CHECKOUT = gql`
   query getCheckout($products: [ID]!) {
     checkout(products: $products) {
